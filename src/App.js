1464b53edaa181c50c6f4bb099e2957b939ba0bc
Chintan Dobariya <chintandobaria18@gmail.com>
2024-05-25 16:29:04 -0500

    did initial graph setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-
+import github from "./db";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    const githubQuery = {
+      query: `
+        {
+          viewer {
+            name
+          }
+        }
+      `
+    };
+
+    fetch(github.baseURL, {
+      method: 'POST',
+      headers: github.headers,
+      body: JSON.stringify(githubQuery)
+    })
+    .then(res => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+  }, [])
+
   return (
-    <div className="App">
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>
+        Repos
+      </h1>
     </div>
   );
 }
